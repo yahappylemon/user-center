87c4d4365b25217353ad9d3fd7b8c40c64b26f6a
yahappygrace <167183109+yahappygrace@users.noreply.github.com>
2024-09-17 17:42:07 +0800

    initialize project

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command }) => {
+  const base = command === "serve" ? "/" : "/user-center/";
+
+  return {
+    base,
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@mui/styled-engine": "@mui/styled-engine-sc",
+      },
+    },
+    define: {
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+    },
+  };
+});
